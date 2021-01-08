201b69d009a075c06827f38204c6a8cd02071da2
aplowman <adam.jp@live.co.uk>
2021-01-08 13:39:51 +0000

    Add Euler/spatial ref frame transformation option

--- matflow_mtex/snippets/get_ODF_from_CRC_file.m
+++ matflow_mtex/snippets/get_ODF_from_CRC_file.m
@@ -1,5 +1,12 @@
-function ODF = get_ODF_from_CRC_file(CRC_file_path, specimenSym, phase)
-    ebsd = loadEBSD_crc(CRC_file_path);
+function ODF = get_ODF_from_CRC_file(CRC_file_path, referenceFrameTransformation, specimenSym, phase)
+    
+    if isempty(referenceFrameTransformation)
+        referenceFrameTransformation = {};
+    else
+        referenceFrameTransformation = {referenceFrameTransformation};
+    end
+    
+    ebsd = loadEBSD_crc(CRC_file_path, referenceFrameTransformation{:});
     ori = ebsd(phase).orientations;
     ori.SS = specimenSymmetry(specimenSym);
     ODF = calcDensity(ori);
