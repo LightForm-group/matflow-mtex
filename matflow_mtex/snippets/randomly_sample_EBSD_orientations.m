4f5a2bdf94f0f9204e5e937ce38aaaa6bfb011ba
APlowman <adam.jp@live.co.uk>
2021-10-26 21:00:55 +0100

    Update SNIPPETS_ARG_TYPES

--- matflow_mtex/snippets/randomly_sample_EBSD_orientations.m
+++ matflow_mtex/snippets/randomly_sample_EBSD_orientations.m
@@ -1,3 +1,3 @@
-function orientations = randomly_sample_EBSD_orientations(EBSD_orientations, num_orientations)
-    orientations = EBSD_orientations(randperm(EBSD_orientations.length, num_orientations));
+function orientations = randomly_sample_EBSD_orientations(EBSD_orientations, numOrientations)
+    orientations = EBSD_orientations(randperm(EBSD_orientations.length, numOrientations));
 end
