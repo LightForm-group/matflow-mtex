c1619829bd2c3454b5dc35d947bd6c1342f7b329
APlowman <adam.jp@live.co.uk>
2022-08-11 14:04:15 +0100

    fix: load_ODF_mat

--- matflow_mtex/snippets/load_ODF_mat.m
+++ matflow_mtex/snippets/load_ODF_mat.m
@@ -1,6 +1,6 @@
 function ODF = load_ODF_mat(ODF_mat_path, crystalSym, specimenSym)
     crystalSym = crystalSymmetry(crystalSym);
-    specimenSym = specimenSymmetry(specimenSym);    
-    ODF = load(ODF_mat_path)
-    );
+    specimenSym = specimenSymmetry(specimenSym);
+    ODF_struct = load(ODF_mat_path);
+    ODF = ODF_struct.odf;
 end
